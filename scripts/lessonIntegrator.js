07810e2d159d0b60d3dc7ecb81f3f94bf72ac79d
🦊 helloyanis <helloyanis@hotmail.com>
2025-03-23 12:18:27 +0100

    Back navigation button works now

--- scripts/lessonIntegrator.js
+++ scripts/lessonIntegrator.js
@@ -3,4 +3,7 @@ async function integrateLesson(category, lesson) {
     const lessonRequest = await fetch(`../data/lessons/${category}/${localStorage.getItem("language")}/${lesson}.html`);
     const lessonData = await lessonRequest.text();
     document.querySelector('#lesson-container').innerHTML = lessonData;
+    const floatingNextButton = document.createElement('md-fab');
+    floatingNextButton.setAttribute('icon', 'arrow_forward');
+    floatingNextButton.setAttribute('color', 'primary');
 }
--- scripts/tabSwitcher.js
+++ scripts/tabSwitcher.js
@@ -2,15 +2,30 @@ document.addEventListener('DOMContentLoaded', function() {
   if(!localStorage.getItem('tab')) {
     localStorage.setItem('tab', 0);
   }
-  document.querySelectorAll('md-primary-tab')[0].removeAttribute('active');
-  document.querySelectorAll('md-primary-tab')[localStorage.getItem('tab')].setAttribute('active', 'true');
-  switchTab(localStorage.getItem('tab'));
+  document.querySelectorAll('md-primary-tab')[localStorage.getItem('tab')].click();
 });
-function switchTab(tab) {
+
+function switchTab(tabIndex) {
   var tabs = document.querySelectorAll('.tab');
   for (var i = 0; i < tabs.length; i++) {
     tabs[i].style.display = 'none';
   }
-  document.querySelectorAll('.tab')[tab].style.display = 'block';
-  localStorage.setItem('tab', tab);
+  document.querySelectorAll('.tab')[tabIndex].style.display = 'block';
+
+  localStorage.setItem('tab', tabIndex);
+
+  var newHash = document.querySelectorAll('.tab')[tabIndex].id;
+
+  if (window.location.hash !== '#' + newHash) {
+    history.pushState({ tab: tabIndex }, null, '#' + newHash);
+  } else {
+    history.replaceState({ tab: tabIndex }, null, '#' + newHash);
+  }
 }
+
+//Detect back button
+window.addEventListener('popstate', function(event) {
+  if (event.state && event.state.tab !== undefined) {
+    document.querySelectorAll('md-primary-tab')[event.state.tab].click();
+  }
+});
