75409b6278bb371cf4e26ebe6d57fe9c37972118
🦊 helloyanis <helloyanis@hotmail.com>
2025-03-24 09:03:44 +0100

    Multilang file interface

--- scripts/lessonIntegrator.js
+++ scripts/lessonIntegrator.js
@@ -1,6 +1,6 @@
 // Utility function to integrate lessons into the main page's lesson container
-async function integrateLesson(category, lesson) {
-    const lessonRequest = await fetch(`../data/lessons/${category}/${localStorage.getItem("language")}/${lesson}.html`);
+async function integrateLesson(category, lesson, section) {
+    const lessonRequest = await fetch(`../data/lessons/${category}/${lesson}/${section}-${localStorage.getItem("language")}.html`);
     const lessonData = await lessonRequest.text();
     document.querySelector('#lesson-container').innerHTML = lessonData;
     const floatingNextButton = document.createElement('md-fab');
