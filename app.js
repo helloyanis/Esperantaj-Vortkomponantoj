feb92520ad082ef9e28a3e1f7867d57af794d808
🦊 helloyanis <helloyanis@hotmail.com>
2025-07-24 09:51:32 +0200

    Fixed action button appearing twice sometimes

--- app.js
+++ app.js
@@ -721,7 +721,6 @@ async function serĉiVorto() {
   serĉoVorto.value = xSistemonSubstituo(serĉoVorto.value)
   const teksto = serĉoVorto.value.trim().toLowerCase();
   document.getElementById('rezulto-karto').innerHTML = '';
-  rezultoFaroj.innerHTML = '';
   if (!teksto) return rezultojSerĉo.innerHTML = 'Bonvolu enigi vorton por serĉi.';
   document.title = `🔍 Serĉo de ${teksto} • VortKom`;
   const url = new URL(location);
@@ -797,7 +796,6 @@ function montriĈipojn(deko) {
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M680-80q-50 0-85-35t-35-85q0-6 3-28L282-392q-16 15-37 23.5t-45 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q24 0 45 8.5t37 23.5l281-164q-2-7-2.5-13.5T560-760q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-24 0-45-8.5T598-672L317-508q2 7 2.5 13.5t.5 14.5q0 8-.5 14.5T317-452l281 164q16-15 37-23.5t45-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-80q17 0 28.5-11.5T720-200q0-17-11.5-28.5T680-240q-17 0-28.5 11.5T640-200q0 17 11.5 28.5T680-160ZM200-440q17 0 28.5-11.5T240-480q0-17-11.5-28.5T200-520q-17 0-28.5 11.5T160-480q0 17 11.5 28.5T200-440Zm480-280q17 0 28.5-11.5T720-760q0-17-11.5-28.5T680-800q-17 0-28.5 11.5T640-760q0 17 11.5 28.5T680-720Zm0 520ZM200-480Zm480-280Z"/></svg>
   </mdui-icon>`;
   butonoDiskonigi.onclick = () => diskonigiURL(deko);
-  rezultoFaroj.appendChild(butonoDiskonigi);
     // Add copy button
   const butonoKopii = document.createElement('mdui-button-icon');
   butonoKopii.alt = 'Kopii ligilon';
@@ -807,6 +805,8 @@ function montriĈipojn(deko) {
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/></svg>
   </mdui-icon>`;
   butonoKopii.onclick = () => kopiiURL(deko);
+  rezultoFaroj.innerHTML = '';
+  rezultoFaroj.appendChild(butonoDiskonigi);
   rezultoFaroj.appendChild(butonoKopii);
 }
 
